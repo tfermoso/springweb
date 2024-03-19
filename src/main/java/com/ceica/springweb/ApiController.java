f39e5bcaee1eecc5e5d0c52b6c1ac56ae11cc012
tfermoso <tomas.fermoso@gmail.com>
2024-03-19 19:46:19 +0100

    peticion datos ajax

--- src/main/java/com/ceica/springweb/ApiController.java
+++ src/main/java/com/ceica/springweb/ApiController.java
@@ -4,12 +4,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 @RequestMapping("/api")
 public class ApiController {
 
-    @GetMapping("/hello")
-    public String prueba(){
-        return "hola mundo";
+    @GetMapping("/pedirdatos")
+    public List<String> prueba(){
+        List<String> datos=new ArrayList<>();
+        datos.add("hola mundo");
+        datos.add("adios mundo");
+        return datos;
     }
 }
--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,5 +1,16 @@
 window.onload = () => {
     document.getElementById("btnPedirDatos").onclick = () => {
 
+        let url = location.origin + "/api/pedirdatos";
+        fetch(url)
+            .then(datos => datos.json())
+            .then(datos => {
+                let lista=document.getElementById("lista");
+                for (let i = 0; i < datos.length; i++) {
+                   let liElement=document.createElement("li");
+                   liElement.textContent=datos[i];
+                   lista.append(liElement);
+                }
+            })
     }
 }
